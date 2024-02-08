89835027531ea3c17c04c76d90e9243570977921
Fanncess <157497061+Fanncess@users.noreply.github.com>
2024-02-08 11:42:58 -0500

    ajout code Encapsulation

--- PO1_Encapsulation/Form1.cs
+++ PO1_Encapsulation/Form1.cs
@@ -2,9 +2,35 @@ namespace PO1_Encapsulation
 {
     public partial class Form1 : Form
     {
+        Personnage joueur = new();
+        Personnage dragon = new Personnage();
         public Form1()
         {
             InitializeComponent();
+            //Personnage personnage = new Personnage();
+            //personnage.PointsDePuissance = 5;
+            //personnage.PointsDevie = -10;
+            //////personnage.Nom = "Flash";
+
+            ////MessageBox.Show(personnage.PointsDevie.ToString());
+            ////if (personnage.EstEnVie )
+            ////{
+
+            ////}
+            joueur.PointsDevie = 10;
+            joueur.PointsDePuissance = 1;
+
+            dragon.PointsDevie = joueur.PointsDevie * 3;
+            dragon.PointsDePuissance = 3;
+
+
+        }
+        private void actualiserLabels()
+        {
+            lbl_pointsVieJoueur.Text = " Vie du joueur : " + joueur.PointsDevie.ToString();
+            lbl_pointsVieDragon.Text = "Vie du dragon : " + dragon.PointsDevie.ToString();
+            lbl_puissanceAttaqueDragon.Text = " Attaque du dragon : " + dragon.PointsDePuissance.ToString();
+            lbl_puissanceAttaqueJoueur.Text = "Attaque du joueur : " + joueur.PointsDePuissance.ToString();
         }
 
         private void btn_attaquer_Click(object sender, EventArgs e)
@@ -20,6 +46,11 @@ namespace PO1_Encapsulation
         private void btn_puissanceAttaque_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            actualiserLabels();
         }
     }
 }
---
+++ PO1_Encapsulation/Personnage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO1_Encapsulation
+{
+    public class Personnage
+    {
+        string nom;
+        int pointsDevie;
+        int pointsDePuissance; // pour proteger nos attributs on fait clic droit ation rapide et refra.... puis encapsuler les champs et utiliser sa proprieté
+
+        public string Nom { get => nom; set => nom = value; }
+        public int PointsDevie
+        {
+            get => pointsDevie;
+
+            set
+            {
+                if (value < 0)
+                {
+                    pointsDevie = 0;
+                }
+                else
+                {
+                    pointsDevie = value;
+                }
+            }
+        }
+
+
+        public int PointsDePuissance { get => pointsDePuissance; set => pointsDePuissance = value; }
+            
+                public bool EstEnVie
+        {
+            get
+            {
+                if (pointsDevie > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
